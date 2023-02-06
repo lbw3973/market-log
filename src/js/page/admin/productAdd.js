4335dc85f5b336659db02b5e75eb091f362fa48d
Dokimion0 <leecy3610@gmail.com>
2023-02-06 23:35:59 +0900

    Feat: 상품추가 기능 구현
    이름, 태그, 가격, 상세설명, 섬네일 입력폼 추가
    섬네일 미리보기 기능
    addProduct함수로 DB에 추가

--- src/js/page/admin/productAdd.js
+++ src/js/page/admin/productAdd.js
@@ -1,3 +1,55 @@
+import { addProduct } from './api.js';
+
 export const productAddHandler = (page) => {
-  console.log('add', page);
+  const form = page.querySelector('.container-form');
+  const productAddBtn = form.querySelector('.container-form__btn--add');
+  const titleInput = form.querySelector(
+    '.container-form__content--title input',
+  );
+  const priceInput = form.querySelector(
+    '.container-form__content--price input',
+  );
+  const dscciptionInput = form.querySelector(
+    '.container-form__content--description textarea',
+  );
+  const tagsSelect = form.querySelector(
+    '.container-form__content--tags select',
+  );
+  const thumbnailInput = form.querySelector(
+    '.container-form__content--thumbnail input',
+  );
+  const preview = document.querySelector(
+    '.container-form__content--thumbnail img',
+  );
+
+  let profileImgBase64 = '';
+
+  thumbnailInput.addEventListener('change', () => {
+    const file = thumbnailInput.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.addEventListener('load', (e) => {
+      console.log(e.target.result);
+      profileImgBase64 = e.target.result;
+      preview.src = e.target.result;
+    });
+  });
+
+  console.log(profileImgBase64)
+
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const product = {
+      title: titleInput.value,
+      price: parseInt(priceInput.value),
+      description: dscciptionInput.value,
+      tags: [tagsSelect.value],
+      thumbnail: profileImgBase64,
+    };
+
+    console.log(product);
+
+    await addProduct(product);
+  });
 };
