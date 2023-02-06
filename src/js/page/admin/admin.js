d5445a7c60143df978a87090e10d90ad46d9e617
Dokimion0 <leecy3610@gmail.com>
2023-02-06 14:32:45 +0900

    Feat: 버튼 페이지네이션 구현
    이전 버튼, 다음 버튼, 페이지 숫자 버튼 구현

--- src/js/page/admin/admin.js
+++ src/js/page/admin/admin.js
@@ -1,4 +1,5 @@
 import Navigo from 'navigo';
+import { getAllProudct } from './api';
 
 let currentPage = 'dashboard';
 
@@ -26,7 +27,7 @@ const renderPage = (html) => {
   const page = content.querySelector('.main-container__content > div');
 
   if (page.className === 'product-container') {
-    // renderPageBtn(page);
+    renderPageBtn(page);
   }
   currentPage = page.dataset.page;
   page.addEventListener('click', pageClickHandler);
@@ -132,9 +133,81 @@ const productAddPage = `
   </div>
 `;
 
-
 const pageClickHandler = (e) => {
+  const page = e.target.closest(`.${currentPage}-container`);
 
+  if (e.target.matches('.products-container__btn-delete')) {
+    console.log('상품 삭제');
+  }
 };
 
 renderPage(dashboardPage);
+
+(async () => {
+  const res = await getAllProudct();
+  console.log(res);
+})();
+
+const renderPageBtn = (page) => {
+  const numberOfProduct = 52; // 제품 배열의 length
+  const itemsPerPage = 10; // 한 페이지에 출력할 제품 수
+  const numberOfPages = Math.ceil(numberOfProduct / itemsPerPage);
+  let pageBtnEl = ``;
+  let activeIdx = 0;
+
+  for (i = 1; i <= numberOfPages; i++) {
+    pageBtnEl += `<button class='product-container__btn-page--number'>${i}</button>`;
+  }
+
+  const productPageBtn = page.querySelector('.product-container__btn-page');
+  productPageBtn.innerHTML = `
+            <button class='product-container__btn-page--prev'>이전</button>
+            ${pageBtnEl}
+            <button class='product-container__btn-page--next'>다음</button>
+      `;
+  const numberBtns = productPageBtn.querySelectorAll(
+    '.product-container__btn-page--number',
+  );
+
+  showCurrentPageNumber(numberBtns, 0, activeIdx);
+
+  numberBtns.forEach((numberBtn, idx) => {
+    numberBtn.addEventListener('click', () => {
+      activeIdx = showCurrentPageNumber(numberBtns, idx);
+    });
+  });
+
+  const prevPageBtn = productPageBtn.querySelector(
+    '.product-container__btn-page--prev',
+  );
+
+  prevPageBtn.addEventListener('click', () => {
+    --activeIdx;
+    if (activeIdx < 0) {
+      activeIdx = 0;
+    }
+    showCurrentPageNumber(numberBtns, activeIdx);
+  });
+
+  const nextPageBtn = productPageBtn.querySelector(
+    '.product-container__btn-page--next',
+  );
+
+  nextPageBtn.addEventListener('click', () => {
+    ++activeIdx;
+
+    if (activeIdx > numberBtns.length - 1) {
+      activeIdx = numberBtns.length - 1;
+    }
+
+    showCurrentPageNumber(numberBtns, activeIdx);
+  });
+};
+
+const showCurrentPageNumber = (numberBtns, idx, activeIdx) => {
+  for (let numberBtn of numberBtns) {
+    numberBtn.classList.remove('active');
+  }
+  numberBtns[idx].classList.add('active');
+  return (activeIdx = idx);
+};
