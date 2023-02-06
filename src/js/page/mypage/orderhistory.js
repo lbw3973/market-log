bfd69c3899fab1d32e88d60b7b2a879a8ddb3a7a
youngeun <jessicak0828@gmail.com>
2023-02-06 14:26:33 +0900

    Feat: 주문내역페이지 상세주문 html

--- src/js/page/mypage/orderhistory.js
+++ src/js/page/mypage/orderhistory.js
@@ -2,3 +2,16 @@ import calendar from './calendar';
 import modal from './modal';
 calendar();
 modal();
+
+async function getOrderHistory() {
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: '',
+  });
+  const json = await res.json();
+  console.log('get', json);
+
+  return json;
+}
+
+window.addEventListener('load', () => {});
