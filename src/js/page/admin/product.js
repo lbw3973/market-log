95055284998b4be04002c350e309323a2e908e9b
dokimion24 <leecy3610@gmail.com>
2023-02-08 20:39:34 +0900

    Feat: 선택한 상품 삭제 기능 추가
    체크박스 선택 시 목록 모두 선택 및 해제 기능
    삭제 버튼 클릭 시 선택한 체크박스 상품 목록 삭제

--- src/js/page/admin/product.js
+++ src/js/page/admin/product.js
@@ -1,8 +1,7 @@
-import { getAllProduct } from './api.js';
+import { addProduct, deleteProduct, getAllProduct } from './api.js';
 let products = [];
 
 export const productHandler = async (page) => {
-  console.log('새로고침');
   products = await getAllProduct();
   const itemsPerPage = 10;
   let activeIdx = 1;
@@ -15,7 +14,7 @@ export const productHandler = async (page) => {
   displayPageBtn(productPageBtn, products, activeIdx, itemsPerPage);
 
   productPageBtn.addEventListener('click', (e) => {
-    if (e.target.classList.contains('product-container__btn')) return;
+    if (e.target.classList.contains('product-container__btn-delete')) return;
 
     if (e.target.classList.contains('product-container__btn-page--number')) {
       let numberBtn = e.target;
@@ -51,8 +50,67 @@ export const productHandler = async (page) => {
     newProducts = paginate(products, activeIdx, itemsPerPage);
     displayProduct(productList, newProducts, activeIdx);
   });
+
+  // 상품 삭제
+  const productContainerBtn = page.querySelector('.product-container__btn');
+  const deleteBtn = productContainerBtn.querySelector(
+    '.product-container__btn-delete',
+  );
+
+  const productsEl = productList.querySelectorAll('li + li');
+
+  const checkProuductAll = productList.querySelector('li:first-child input');
+  checkProuductAll.addEventListener('change', () => {
+    checkProuductAll.checked
+      ? [...productsEl].forEach(
+          (productEl) => (productEl.querySelector('input').checked = true),
+        )
+      : [...productsEl].forEach(
+          (productEl) => (productEl.querySelector('input').checked = false),
+        );
+  });
+
+  deleteBtn.addEventListener('click', async () => {
+    const newProductsEl = [...productsEl].filter(
+      (productEl) => productEl.querySelector('input').checked === true,
+    );
+
+    if (newProductsEl.length === 0) {
+      alert('상품을 선택해주세요.');
+      return;
+    }
+
+    if (confirm('선택한 상품을 삭제하시겠습니까? ')) {
+      await Promise.all(
+        newProductsEl.map(
+          async (newProductEl) => await deleteProduct(newProductEl.dataset.id),
+        ),
+      );
+
+      products = await getAllProduct();
+      productList.innerHTML = `
+      <li>
+        <input type="checkbox">
+        <span style='width: 5%;'>NO</span>
+        <span style='width: 10%;'>카테고리</span>
+        <span style='width: 10%;'>상품명</span>
+        <span style='width: 15%;'>가격</span>
+        <span style='width: 15%;'>품절여부</span>
+      </li>
+    `;
+
+      alert('선택한 상품이 삭제되었습니다.');
+    } else {
+      return;
+    }
+    displayPageBtn(productPageBtn, products, activeIdx, itemsPerPage);
+    newProducts = paginate(products, activeIdx, itemsPerPage);
+    console.log(newProducts);
+    displayProduct(productList, newProducts, activeIdx);
+  });
 };
 
+//함수 정의
 const displayPageBtn = (productPageBtn, products, activeIdx, itemsPerPage) => {
   let buttonsEl = ``;
 
@@ -79,6 +137,7 @@ const paginate = (products, activeIdx, itemsPerPage) => {
 const displayProduct = (productList, products, activeIdx) => {
   const productsEl = products.map((product, idx) => {
     const productEl = document.createElement('li');
+    productEl.dataset.id = `${product.id}`;
     productEl.innerHTML = `
           <input type="checkbox">
           <span style='width: 5%;'>${idx + 1 + (activeIdx - 1) * 10}</span>
