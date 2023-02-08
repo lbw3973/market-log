3c10a365bcd5d66869bae7426ef9ab4f79ff4eca
dokimion24 <leecy3610@gmail.com>
2023-02-08 20:37:37 +0900

    Feat: headers의 username 수정, 주석 삭제

--- src/js/page/admin/api.js
+++ src/js/page/admin/api.js
@@ -1,13 +1,13 @@
 const headers = {
   'content-type': 'application/json',
   apikey: 'FcKdtJs202301',
-  username: 'KDT4_TEAM3',
+  username: 'KDT4_Team3',
   masterKey: true,
 };
 
 export const getAllProduct = async () => {
   const res = await fetch(
-    'https://asia-northeast3-heropy-api.cloudfunctions.net/api/products ',
+    'https://asia-northeast3-heropy-api.cloudfunctions.net/api/products',
     {
       method: 'GET',
       headers,
@@ -19,7 +19,7 @@ export const getAllProduct = async () => {
 
 export const addProduct = async (product) => {
   await fetch(
-    'https://asia-northeast3-heropy-api.cloudfunctions.net/api/products ',
+    'https://asia-northeast3-heropy-api.cloudfunctions.net/api/products',
     {
       method: 'POST',
       headers,
@@ -34,23 +34,12 @@ export const addProduct = async (product) => {
   );
 };
 
-export const deleteProduct = async (product) => {
-  const res = await fetch(
-    `https://asia-northeast3-heropy-api.cloudfunctions.net/api/${id}`,
+export const deleteProduct = async (id) => {
+  await fetch(
+    `https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/${id}`,
     {
-      method: 'POST',
+      method: 'DELETE',
       headers,
     },
   );
 };
-
-// {
-//   id: '2fYWw6890PmjFLFwjp7C',
-//   title: 'OTG 어댑터 OTG-03',
-//   price: 6770,
-//   description: '컴퓨터 연결없이 데이터 전송~',
-//   tags: ['smart', 'black', 'new-item', 'discount', 'adapter'],
-//   thumbnail:
-//     'https://storage.googleapis.com/heropy-api/vJJfrkM51Qv060726.jpg',
-//   isSoldOut: false,
-// },
