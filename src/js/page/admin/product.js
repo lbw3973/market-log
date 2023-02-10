e392b3251764953d669c1877f9ad5566c56addcb
dokimion24 <leecy3610@gmail.com>
2023-02-10 10:06:58 +0900

    Feat: 검색 필터 1차 구현
    태그, 품절여부 선택하여 각각 따로 기능구현은 되지만 같이 사용할 경우
    작동하지 않음. 수정 필요

--- src/js/page/admin/product.js
+++ src/js/page/admin/product.js
@@ -1,7 +1,9 @@
-import { addProduct, deleteProduct, getAllProduct } from './api.js';
+import { deleteProduct, getAllProduct, editProduct } from './api.js';
 let products = [];
+let selectedProducts = [];
 
 export const productHandler = async (page) => {
+  // 초기화
   products = await getAllProduct();
   const itemsPerPage = 10;
   let activeIdx = 1;
@@ -11,10 +13,73 @@ export const productHandler = async (page) => {
   const checkProductAll = page.querySelector('.product-container__title input');
 
   displayProduct(productList, newProducts, activeIdx);
-
   const productPageBtn = page.querySelector('.product-container__btn-page');
   displayPageBtn(productPageBtn, products, activeIdx, itemsPerPage);
 
+  //상품 검색
+  const searchContainer = page.querySelector(
+    '.product-container__search-container',
+  );
+
+  searchContainer.addEventListener('change', (e) => {
+    let selectedTagsProducts;
+    let selectedSoldOutProducts;
+
+    if (e.target.classList.contains('tags')) {
+      selectedTagsProducts = selectOptionValue(e, selectedProducts) || products;
+    }
+
+    if (e.target.classList.contains('soldout')) {
+      selectedSoldOutProducts =
+        selectOptionValue(e, selectedProducts) || products;
+    }
+
+    console.log('tag', selectedTagsProducts);
+    console.log('soldout', selectedSoldOutProducts);
+    selectedProducts = [
+      ...new Set([
+        ...(selectedTagsProducts || []),
+        ...(selectedSoldOutProducts || []),
+      ]),
+    ];
+
+    productList.innerHTML = ``;
+    displayPageBtn(productPageBtn, selectedProducts, activeIdx, itemsPerPage);
+    const newProducts = paginate(selectedProducts, activeIdx, itemsPerPage);
+    displayProduct(productList, newProducts, activeIdx);
+    // if (e.target.classList.contains('button')) {
+    //   console.log(searchedProducts);
+
+    //   if (!selectedProducts) {
+    //     selectedProducts = products;
+    //   }
+    //   const keyword = searchContainer.querySelector('input').value;
+    //   const searchedProducts = findSearchedProduct(selectedProducts, keyword);
+
+    //   productList.innerHTML = ``;
+    //   displayPageBtn(productPageBtn, searchedProducts, activeIdx, itemsPerPage);
+    //   newProducts = paginate(searchedProducts, activeIdx, itemsPerPage);
+    //   displayProduct(productList, newProducts, activeIdx);
+    // }
+  });
+
+  // categorySelect.addEventListener('click', (e) => {
+  //   const selectedProducts = selectOptionValue(e);
+  // });
+
+  // souldOutSelect.addEventListener('click', (e) => {
+  //   const selectedProducts = selectOptionValue(e);
+  // });
+
+  // searchProductBtn.addEventListener('click', () => {
+  //   console.log('검색');
+  //   const searchInput = searchContainer.querySelector('input');
+  //   const keyword = searchInput.value;
+  //   const searchedProducts = findSearchedProduct(selectedProducts, keyword);
+  //   console.log(searchedProducts);
+  // });
+
+  //버튼 클릭 페이지 이동
   productPageBtn.addEventListener('click', (e) => {
     if (e.target.classList.contains('product-container__btn-delete')) return;
 
@@ -37,16 +102,14 @@ export const productHandler = async (page) => {
       }
     }
 
-    productList.innerHTML = `
-    
-    `;
+    productList.innerHTML = ``;
 
     displayPageBtn(productPageBtn, products, activeIdx, itemsPerPage);
     newProducts = paginate(products, activeIdx, itemsPerPage);
     displayProduct(productList, newProducts, activeIdx);
   });
 
-  // 상품 삭제
+  // 체크리스트 모두 선택 및 해제
   const productContainerBtn = page.querySelector('.product-container__btn');
   const deleteBtn = productContainerBtn.querySelector(
     '.product-container__btn-delete',
@@ -64,15 +127,13 @@ export const productHandler = async (page) => {
         );
   });
 
+  // 체크리스트 선택한 상품 삭제
   deleteBtn.addEventListener('click', async () => {
     const productsEl = productList.querySelectorAll('li');
 
-    console.log('pEl', [...productsEl]);
     const newProductsEl = [...productsEl].filter(
       (productEl) => productEl.querySelector('input').checked === true,
     );
-
-    console.log('nEl', newProductsEl);
 
     if (newProductsEl.length === 0) {
       alert('상품을 선택해주세요.');
@@ -135,7 +196,7 @@ const displayProduct = (productList, products, activeIdx) => {
           <span style='width: 10%;'>${product.title}</span>
           <span style='width: 15%;'>${product.price.toLocaleString()} 원</span>
           <span style='width: 15%;'>${
-            'true' === product.isSoldOut ? '품절' : '판매가능'
+            product.isSoldOut ? '품절' : '판매가능'
           }</span>
         `;
 
@@ -148,3 +209,26 @@ const displayProduct = (productList, products, activeIdx) => {
 (async () => {
   products = await getAllProduct();
 })();
+
+const selectOptionValue = (e, selectedProducts) => {
+  if (!e.target.value) {
+    return;
+  }
+  let filtered;
+  if (e.target.name === 'tags') {
+    filtered = products.filter((product) => product.tags[0] === e.target.value);
+  } else if (e.target.name === 'soldout') {
+    filtered = products.filter(
+      (product) => String(product.isSoldOut) === e.target.value,
+    );
+  }
+
+  return filtered;
+};
+
+const findSearchedProduct = (products, keyword) => {
+  if (!products) return;
+  return products.filter((product) => product.title.includes(keyword));
+};
+
+const clickSearchInputHandler = (e, searchContainer) => {};
