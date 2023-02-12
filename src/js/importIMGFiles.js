02573b326cf77e82e9e65d5fae91263fca48fdbb
Jaeha Lee <wogkdkrm112@gmail.com>
2023-02-12 17:34:17 +0900

    Feat: [메인페이지] template 루트 경로 삽입
    메인페이지 템플릿 navigo '/' 라우팅에 등록

---
+++ src/js/importIMGFiles.js
@@ -0,0 +1,7 @@
+import air60 from '../../public/mainPageIMG/air60.png';
+import air75 from '../../public/mainPageIMG/air75.png';
+import halo65 from '../../public/mainPageIMG/halo65.png';
+import halo75 from '../../public/mainPageIMG/halo75.png';
+import halo96 from '../../public/mainPageIMG/halo96.png';
+
+export { air60, air75, halo65, halo75, halo96 };
--- src/js/testJaeha.js
+++ src/js/testJaeha.js
@@ -1,6 +1,7 @@
 import Navigo from 'navigo';
 const router = new Navigo('/');
 const $ = (selector) => document.querySelector(selector);
+import { air60, air75, halo65, halo75, halo96 } from './importIMGFiles.js';
 
 /** 렌더 함수 for navigo */
 const renderPage = (html) => {
@@ -18,7 +19,107 @@ const HEADERS = {
   #메인 페이지
 \*-----------------------------------*/
 
-// renderPage()
+const renderMainPageTemplate = `
+<div class="mainPage">
+  <div class="mainPage__container">
+    <div class="mainPage__content">
+      <div class="mainPage__hero">
+        <a href="/category">
+          <img
+            class="mainPage__hero--img"
+            src="${halo96}"
+            alt=""
+          />
+          <div class="mainPage__hero--info-container">
+            <h2 class="mainPage__hero--title">Halo 96</h2>
+            <div class="mainPage__hero--desc">Shortcut your 9 to 6</div>
+            <button class="mainPage__hero--btn">BUY NOW</button>
+          </div>
+        </a>
+      </div>
+      <!-- Halo 75 -->
+      <div class="mainPage__hero">
+        <a href="/category">
+          <img
+            class="mainPage__hero--img"
+            src="${halo75}"
+            alt=""
+          />
+          <div class="mainPage__hero--info-container">
+            <h2 class="mainPage__hero--title">Halo 75</h2>
+            <div class="mainPage__hero--desc">
+              New heights, new lights, and new highlights
+            </div>
+            <button class="mainPage__hero--btn">BUY NOW</button>
+          </div>
+        </a>
+      </div>
+
+      <!-- Halo 65 -->
+      <div class="mainPage__hero">
+        <a href="/category">
+          <div class="mainPage__img-container">
+            <img
+              class="mainPage__hero--img"
+              src="${halo65}"
+              alt=""
+            />
+          </div>
+          <div class="mainPage__hero--info-container">
+            <h2 class="mainPage__hero--title">Halo 65</h2>
+            <div class="mainPage__hero--desc">
+              New heights, new lights, and new highlights
+            </div>
+            <button class="mainPage__hero--btn">BUY NOW</button>
+          </div>
+        </a>
+      </div>
+
+      <!-- Air 60 -->
+      <div class="mainPage__hero">
+        <a href="/category">
+          <div class="mainPage__img-container">
+            <img
+              class="mainPage__hero--img"
+              src="${air60}"
+              alt=""
+            />
+          </div>
+          <div class="mainPage__hero--info-container">
+            <h2 class="mainPage__hero--title">Air 60</h2>
+            <div class="mainPage__hero--desc">
+              Revolutionizing the laptop experience
+            </div>
+            <button class="mainPage__hero--btn">BUY NOW</button>
+          </div>
+        </a>
+      </div>
+
+      <!-- Air 75 -->
+      <div class="mainPage__hero">
+        <a href="/category">
+          <div class="mainPage__img-container">
+            <img
+              class="mainPage__hero--img"
+              src="${air75}"
+              alt=""
+            />
+          </div>
+          <div class="mainPage__hero--info-container">
+            <h2 class="mainPage__hero--title">Air 75</h2>
+            <div class="mainPage__hero--desc">
+              The world's thinnest mechanical keyboard
+            </div>
+            <button class="mainPage__hero--btn">BUY NOW</button>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+renderPage(renderMainPageTemplate);
 
 /*-----------------------------------*\
   #메인 페이지에 상세 제품 불러오기 테스트
@@ -596,6 +697,7 @@ import {
   wooriBank,
 } from '../js/page/pay/payIMG.js';
 import Swiper, { Navigation, Pagination } from 'swiper';
+import { render } from 'sass';
 Swiper.use([Navigation, Pagination]);
 
 const buyItemAPI = async (productId, accountId) => {
@@ -986,6 +1088,7 @@ router
     '/': async () => {
       $('.modal__addCart').style.display = 'none';
       console.log('/ route is working');
+      renderPage(renderMainPageTemplate);
       // await initializeMainPage();
     },
     '/product/:id': async (params) => {
