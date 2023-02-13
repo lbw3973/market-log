cf4c9fcfebc5c0b12e675c4a33d62c5091f11d62
dokimion24 <leecy3610@gmail.com>
2023-02-13 13:54:54 +0900

    Feat: api 함수 작성 및 수정
    editProduct 파라미터 변경
    상품상제정보 가져오는 함수 -getDetailProduct
    전체상품거래내역 가져오는 함수 -getAllOrder
    작업중 - editDoneOrder, editCancelOrder

--- src/js/page/admin/api.js
+++ src/js/page/admin/api.js
@@ -44,15 +44,90 @@ export const deleteProduct = async (id) => {
   );
 };
 
-export const editProduct = async (id) => {
+export const editProduct = async (product) => {
   await fetch(
-    `https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/${id}`,
+    `https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/${product.id}`,
     {
       method: 'PUT',
       headers,
       body: JSON.stringify({
-        isSoldOut: true,
+        title: product.title,
+        price: product.price,
+        description: product.description,
+        tags: product.tags,
+        isSoldOut: product.isSoldOut,
+        thumbnailBase64: product.thumbnail,
       }),
     },
   );
+};
+
+export const getDetailProduct = async (id) => {
+  try {
+    const res = await fetch(
+      `https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/${id}`,
+      {
+        method: 'GET',
+        headers,
+      },
+    );
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    console.log('err: ', '해당 제품을 불러오기 실패');
+  }
+};
+
+export const getAllOrder = async () => {
+  try {
+    const res = await fetch(
+      `https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/transactions/all`,
+      {
+        method: 'GET',
+        headers,
+      },
+    );
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    console.log('err: ', '전체 거래 내역 불러오기 실패');
+  }
+};
+
+export const editDoneOrder = async (detailId) => {
+  try {
+    await fetch(
+      `https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/transactions/${detailId}`,
+      {
+        method: 'PUT',
+        headers,
+        body: JSON.stringify({
+          done: !done,
+        }),
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    console.log('err: ', '거래 내역 완료 실패');
+  }
+};
+
+export const editCancelOrder = async (detailId) => {
+  try {
+    await fetch(
+      `https://asia-northeast3-heropy-api.cloudfunctions.net/api/products/transactions/${detailId}`,
+      {
+        method: 'PUT',
+        headers,
+        body: JSON.stringify({
+          isCanceled: !isCanceled,
+        }),
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    console.log('err: ', '거래 내역 취소 실패');
+  }
 };
