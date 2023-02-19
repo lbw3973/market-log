0db007d3f8b907fb54ee17d908a153ff2e695829
dokimion24 <leecy3610@gmail.com>
2023-02-19 19:01:08 +0900

    Feat: testChanYoung.js 상품상세 페이지 핸들러 함수 분리
    productEdit.js로 이동

---
+++ src/js/page/admin/productEdit.js
@@ -0,0 +1,72 @@
+import { editProduct, getDetailProduct } from './api';
+import { router } from '../../testChanYoung.js';
+import { renderEditProduct } from './renderDetail';
+
+/** 상품수정 페이지 핸들러 */
+export const productEditHandler = async (productId) => {
+  const product = await getDetailProduct(productId);
+
+  document
+    .querySelector('.productEdit-container')
+    .insertAdjacentHTML('afterbegin', renderEditProduct(product));
+
+  console.log(product);
+
+  const form = document.querySelector('.container-form');
+
+  const titleInput = form.querySelector(
+    '.container-form__content--title input',
+  );
+  const priceInput = form.querySelector(
+    '.container-form__content--price input',
+  );
+  const desciptionInput = form.querySelector(
+    '.container-form__content--description textarea',
+  );
+  const tagsSelect = form.querySelector(
+    '.container-form__content--tags select',
+  );
+  const soldoutSelect = form.querySelector(
+    '.container-form__content--soldout select',
+  );
+  const thumbnailInput = form.querySelector(
+    '.container-form__content--thumbnail input',
+  );
+  const preview = document.querySelector(
+    '.container-form__content--thumbnail img',
+  );
+
+  let profileImgBase64 = ``;
+
+  thumbnailInput.addEventListener('change', () => {
+    const file = thumbnailInput.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.addEventListener('load', (e) => {
+      profileImgBase64 = e.target.result;
+      preview.src = e.target.result;
+    });
+  });
+
+  document
+    .querySelector('.container-form__btn--edit')
+    .addEventListener('click', async () => {
+      const product = {
+        id: productId,
+        title: titleInput.value,
+        price: parseInt(priceInput.value),
+        description: desciptionInput.value,
+        tags: [tagsSelect.value],
+        isSoldOut: 'true' === soldoutSelect.value,
+        thumbnail: profileImgBase64,
+      };
+
+      console.log(product);
+
+      if (confirm('상품을 수정하시겠습니까?')) {
+        await editProduct(product);
+        alert('상품이 수정되었습니다.');
+        router.navigate(`/admin/product/${productId}`);
+      }
+    });
+};
