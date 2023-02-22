2b01b597833958d2b89d1af068666d83b95819ea
bwlee <lbw3973@naver.com>
2023-02-22 18:58:56 +0900

    Feat: 개인정보 수정 구현 완료

--- src/js/page/mypage/editPersonalInfo.js
+++ src/js/page/mypage/editPersonalInfo.js
@@ -1,12 +1,22 @@
 import { $ } from '../../utils/dom.js';
+import { base_url, api_key, user_name } from '../../db.js';
 import { router } from '../../main.js';
 import { renderPage } from '../../utils/render.js';
 import { getLoginStatus, showAlertPlzLogin } from '../login.js';
 import { htmlMypage_Nav, resetNavbarActive } from '../mypage.js';
-
+import {
+  authorization,
+  personalInfoLogin,
+  submitChangeInfo,
+} from '../../api.js';
+const headers = {
+  'content-type': 'application/json',
+  apikey: api_key,
+  username: user_name,
+};
 /** 개인정보 수정 페이지 html 템플릿 */
-const handleeditPersonalInfoInitTemplate = (success) => {
-  const htmlMypage_editPersonalInfo_input = /* html */ `
+const handleEditPersonalInfoPrecheckTemplate = () => {
+  const htmlMypage_editPersonalInfo_precheck = /* html */ `
   <h2>개인 정보 수정</h2>
   <div class="mypage__editPersonalInfo">
     <div class="mypage__editPersonalInfo__notice">
@@ -19,19 +29,24 @@ const handleeditPersonalInfoInitTemplate = (success) => {
     </div>
   </div>
   `;
-
+  $('.mypage__navigo__container').innerHTML =
+    htmlMypage_editPersonalInfo_precheck;
+};
+const handleEditPersonalInfoTemplate = (email, displayName) => {
   const htmlMypage_editPersonalInfo_edit = /* html */ `
     <h2>개인 정보 수정</h2>
     <div class="edit_PersonalInfo">
       <div class="edit_PersonalInfo_grid">
         <label>아이디</label>
-        <p id="user-id">예시! 지우고 변수로 받으셔야합니다</p>
-        <label>비밀번호</label>
-        <input id="user-pw" placeholder="비밀번호"/>
-        <label>비밀번호 확인</label>
-        <input id="user-pwConfirm" placeholder="비밀번호 확인"/>
+        <p id="user-id">${email}</p>
+        <label>기존 비밀번호</label>
+        <input type="password" id="user-oldpw" placeholder="기존 비밀번호"/>
+        <label>새 비밀번호</label>
+        <input type="password" id="user-newpw" placeholder="새로운 비밀번호"/>
+        <label>새 비밀번호 확인</label>
+        <input type="password" id="user-newpwConfirm" placeholder="새로운 비밀번호 확인"/>
         <label>닉네임 변경</label>
-        <input id="user-name" placeholder="닉네임, 여기는 페이지 렌더될때 value속성으로 기존 닉네임 가져오기"/>
+        <input id="user-name" value="${displayName}"/>
       </div>
       <div class="buttonBox">
         <button id="btnEditComplete">정보 수정</button>
@@ -39,12 +54,7 @@ const handleeditPersonalInfoInitTemplate = (success) => {
       </div>
     </div>
   `;
-  // 밑에 주석은 테스트용
   $('.mypage__navigo__container').innerHTML = htmlMypage_editPersonalInfo_edit;
-
-  // $('.mypage__navigo__container').innerHTML = success
-  //   ? htmlMypage_editPersonalInfo_edit
-  //   : htmlMypage_editPersonalInfo_input;
 };
 
 /** 처음 진입점 */
@@ -54,28 +64,61 @@ export const handleeditPersonalInfoPage = () => {
     router.navigate('/login');
     return;
   }
-  rendereditPersonInfoPage();
+  renderEditPersonalInfoPage();
+  //mypage navbar
   resetNavbarActive();
   setNavbarActive();
 };
+
 /** 페이지 렌더 및 기능 부여 */
-const rendereditPersonInfoPage = () => {
+const renderEditPersonalInfoPage = () => {
   renderPage(htmlMypage_Nav);
-  handleeditPersonalInfoInitTemplate(false);
-  initFunceditPersonalInfo();
+  handleEditPersonalInfoPrecheckTemplate();
+  checkPersonalPassword();
 };
 
-/** 기능 부여 */
-function initFunceditPersonalInfo() {}
-
+// 비밀번호 재확인 페이지 함수
+function checkPersonalPassword() {
+  $('#btnSubmit').addEventListener('click', async () => {
+    const auth = await authorization();
+    const res = await personalInfoLogin(auth);
+    res.accessToken ? page(auth) : alert('비밀번호가 잘못되었습니다');
+  });
+  $('#inputPW').addEventListener('keydown', async (e) => {
+    if (e.key === 'Enter') {
+      $('#btnSubmit').click();
+    }
+  });
+}
+const page = function (auth) {
+  handleEditPersonalInfoTemplate(auth.email, auth.displayName);
+  //사용자 정보 수정 submit
+  $('#btnEditComplete').addEventListener('click', async () => {
+    if (
+      !$('#user-newpwConfirm').value ||
+      !$('#user-oldpw').value ||
+      !$('#user-newpw').value ||
+      !$('#user-name').value
+    ) {
+      alert('내용을 전부 기입해주세요');
+      return;
+    } else if ($('#user-newpw').value !== $('#user-newpwConfirm').value) {
+      alert('새로운 비밀번호가 일치하지 않습니다');
+      return;
+    }
+    const res = await submitChangeInfo();
+    if (res.email) {
+      alert('수정이 완료되었습니다');
+      await router.navigate('/');
+    } else {
+      alert(res);
+    }
+  });
+  $('#btnEditCancel').addEventListener('click', () => {
+    window.location.reload();
+  });
+};
 const setNavbarActive = () => {
   const active = document.querySelector('#mpEditPersonalInfo');
   active.parentElement.classList.add('active');
 };
-
-/*
-1. 비밀번호를 입력하면 edit page를 렌더
-  - 현재 로그인 되어있는 아이디를 불러와서 다시 로그인
-  - 로그인이 성공하면 edit page를 렌더
-2. edit page에서 
-*/
