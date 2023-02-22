3017a437f02399b90084dd24561f51067695a764
dokimion24 <leecy3610@gmail.com>
2023-02-22 15:50:02 +0900

    Refactor: 로딩 애니메이션 코드 수정

--- src/js/utils/loading.js
+++ src/js/utils/loading.js
@@ -1,7 +1,4 @@
 export const toggleLoadingSpinner = (isActive) => {
   const LoadingSpinner = document.querySelector('.loadingGif');
-  console.log(LoadingSpinner);
-  isActive
-    ? (LoadingSpinner.style.display = 'block')
-    : (LoadingSpinner.style.display = 'none');
+  LoadingSpinner.style.display = isActive ? 'block' : 'none';
 };
