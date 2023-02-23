dd7f88520ff5f2fd6f2dd85f27647871b55b95be
Jaeha Lee <wogkdkrm112@gmail.com>
2023-02-23 20:01:13 +0900

    Feat: 계좌 연결 api interface 수정

--- src/ts/interface/index.ts
+++ src/ts/interface/index.ts
@@ -59,10 +59,18 @@ export interface CancelConfirmTransactionAPI {
 
 /** 계좌연결 */
 export interface GetAccountDetail {
-  bankCode: string; // 연결할 은행 코드 (필수!)
-  accountNumber: string; // 연결할 계좌번호 (필수!)
-  phoneNumber: string; // 사용자 전화번호 (필수!)
-  signature: boolean; // 사용자 서명 (필수!)
+  // [x: string]: number;
+  totalBalance?: number; // 사용자 계좌 잔액 총합
+  accounts: Bank[]; // 사용자 계좌 정보 목록
+}
+
+export interface Bank {
+  // 사용자 계좌 정보
+  id: string; // 계좌 ID
+  bankName: string; // 은행 이름
+  bankCode: string; // 은행 코드
+  accountNumber: string; // 계좌 번호
+  balance: number; // 계좌 잔액
 }
 
 /** 제품 결제 API */
