e843bdb377ed3a4bc376e5ab473a0e7bc1ca8a65
Jaeha Lee <wogkdkrm112@gmail.com>
2023-02-23 17:45:36 +0900

    Feat: [store.ts] WishListStore interface 추가

--- src/ts/interface/index.ts
+++ src/ts/interface/index.ts
@@ -15,7 +15,7 @@ export interface GetAllProductsInterface {
   id: string; // 제품 ID
   title: string; // 제품 이름
   price: number; // 제품 가격
-  description: string; // 제품 설명(최대 100자)
+  description?: string; // 제품 설명(최대 100자)
   tags: string[]; // 제품 태그
   thumbnail?: string | null; // 제품 썸네일 이미지(URL)
   isSoldOut?: boolean; // 제품 매진 여부
--- src/ts/interface/store.ts
+++ src/ts/interface/store.ts
@@ -4,8 +4,19 @@ export interface ShoppingCartStore {
   count: number;
   id: string;
   price?: number;
+  qty?: number;
   pricePerOne: number;
   thumbnail: string;
   title: string;
 }
 export type ShoppingCartStoreValue = ShoppingCartStore[];
+
+export interface WishListStore {
+  count: number;
+  id: string;
+  price?: number;
+  pricePerOne: number;
+  thumbnail: string;
+  title: string;
+}
+export type WishListStoreValue = WishListStore[];
