e3e9cff9df5588447b1902999aafc1451a2aac68
youngeun <jessicak0828@gmail.com>
2023-02-24 14:54:44 +0900

    Fix: boolean값으로 변경

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -1,6 +1,6 @@
 import {
   AddProduct,
-  AuthorizationValue,
+  Authorization,
   Bank,
   BuyItemAPI,
   CancelConfirmTransactionAPI,
@@ -419,7 +419,7 @@ export async function login() {
 }
 
 /** API : Logout */
-export async function logout(): Promise<Logout> {
+export async function logout(): Promise<boolean> {
   const res = await fetch(`${base_url}/auth/logout`, {
     method: 'POST',
     headers: tokenHeaders,
@@ -429,7 +429,7 @@ export async function logout(): Promise<Logout> {
 }
 
 /** API : 인증확인 */
-export async function authorization(): Promise<AuthorizationValue> {
+export async function authorization(): Promise<Authorization> {
   const res = await fetch(`${base_url}/auth/me`, {
     method: 'POST',
     headers: tokenHeaders,
