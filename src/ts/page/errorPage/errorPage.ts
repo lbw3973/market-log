f90af974b5345a2f4b17efa173229bcce40d720c
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-05 15:48:59 +0900

    Feat: [errorPage] 홈으로 돌아가기 버튼 추가

--- src/ts/page/errorPage/errorPage.ts
+++ src/ts/page/errorPage/errorPage.ts
@@ -5,8 +5,10 @@ export const handleErrorPage = () => {
 <div class="error-page">
     <div class="error-message--container">
       <p class="error-message--title">원하시는 페이지를 찾을 수 없습니다 ;(</p>
-      <p class="error-message--desc">찾으려는 페이지의 주소가 잘못 입력되었거나,주소의 변경 혹은 삭제로 인해 사용하실 수 없습니다.<br>입력하신 페이지의 주소가 정확한지 다시 한번 확인해 주세요.</p>
+      <p class="error-message--desc">찾으려는 페이지의 주소가 잘못 입력되었거나,주소의 변경 혹은 삭제로 인해 사용하실 수 없습니다.<br>입력하신 페이지의 주소가 정확한지 다시 한번 확인해 주세요.</p></br></br></br></br>
+      <a href="/">홈으로 돌아가기</a>
     </div>
+    
 </div>
 `;
   renderPage(htmlErrorPage);
