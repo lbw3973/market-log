c1dae4c9b308ac197cec9ad284af5f9407d004ef
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-05 15:54:32 +0900

    Refactor: mypage 리팩토링
    mypage/index.ts -> mypage/mypageIMG.ts 파일명 수정

--- src/ts/page/mypage/mypageIMG.ts
+++ src/ts/page/mypage/mypageIMG.ts
@@ -3,9 +3,5 @@ import exclamationmark from '../../../../public/exclamationmark.svg';
 import paginationLeft from '../../../../public/pagination-left.svg';
 import paginationRight from '../../../../public/pagination-right.svg';
 import calendar from '../../../../public/calendar.svg';
-// import { reload } from '../../../../public/reload.svg';
-// import { reload } from '../../../../public/reload.svg';
-// import { reload } from '../../../../public/reload.svg';
-// import { reload } from '../../../../public/reload.svg';
 
 export { reload, exclamationmark, paginationLeft, paginationRight, calendar };
